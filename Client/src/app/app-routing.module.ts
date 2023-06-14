d6b11ae08c02232eac8f1ea13f64f5e0f0898ca0
EladArbook <eladarbook21@gmail.com>
2023-06-14 13:46:25 +0300

    menu added

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Components/about/about.component';
+import { AppointmentComponent } from './Components/appointment/appointment.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { AuthComponent } from './Components/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'appointment',
+    component: AppointmentComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { AppointmentComponent } from './Components/appointment/appointment.component';
+import { AuthComponent } from './Components/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    AppointmentComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
