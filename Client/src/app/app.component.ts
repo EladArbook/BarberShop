ba612ad1867650479aaef85802b631777048a939
EladArbook <eladarbook21@gmail.com>
2023-06-15 10:50:40 +0300

    Added router, modal-box for pages, responsive menu for phones.

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  openPhoneMenu() {//fade in
+
+    const menuBar = document.getElementById('menu');
+    if (menuBar != null) {
+      if (menuBar.style.opacity === '1') {//fade out
+        menuBar.style.pointerEvents = "none";
+        menuBar.style.opacity = '0';
+      }
+      else {//fade in
+        menuBar.style.pointerEvents = "auto";
+        menuBar.style.opacity = '1';
+      }
+    }
+  }
+
+  closeMenu() {
+    const menuBar = document.getElementById('menu');
+    if (menuBar != null) {
+      if (menuBar.style.opacity === '1') {//fade out
+        menuBar.style.pointerEvents = "none";
+        menuBar.style.opacity = '0';
+      }
+    }
+  }
 }
